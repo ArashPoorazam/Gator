19d2dfa629bee702c4397b2b1a578b232337db60
ArashPoorazam <arashpoorazam88@gmail.com>
2025-12-22 22:37:35 +0330

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,76 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const configFilePath = "/bootdev/Gator/.gatorconfig.json"
+
+type Config struct {
+	Current_user_name string `json:"current_user_name"`
+	Db_url            string `json:"db_url"`
+}
+
+func (cfg *Config) SetUser(username string) (err error) {
+	cfg.Current_user_name = username
+
+	err = write(*cfg)
+	if err != nil {
+		return fmt.Errorf("could not write file, error: %w", err)
+	}
+
+	return nil
+}
+
+func ReadConfig() (config Config, err error) {
+	address, err := getConfigFilePath()
+	if err != nil {
+		return config, err
+	}
+
+	data, err := os.ReadFile(address)
+	if err != nil {
+		return config, fmt.Errorf("could not read file, error: %w", err)
+	}
+
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return config, fmt.Errorf("could not read file, error: %w", err)
+	}
+
+	return config, nil
+}
+
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("could not find home directory, error: %w", err)
+	}
+
+	fullPath := filepath.Join(homeDir, configFilePath)
+	return fullPath, nil
+}
+
+func write(cfg Config) error {
+	fullPath, err := getConfigFilePath()
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- mian.go
+++ mian.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/ArashPoorazam/Gator/internal/config"
 )
 
 func main() {
 	fmt.Println("Hello world!")
+
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cfg.SetUser("Arash Poorazam")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("config: %+v\n", cfg)
 }
